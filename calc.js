b8e41f828b5fcd42c66886b8381b24876e0781ac
Hideaki Fukami <denys.fukami@gmail.com>
2023-01-26 18:08:33 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -20,4 +20,137 @@ let numeroAnterior
 let numeroAtual
 let apagarIgual
 
+const ajustaPontoVirgula = () => display.textContent = display.textContent.replace('.', ',')
+
+const inserirDisplay = text => {
+    
+    if (primeiro) {
+        display.textContent = text
+        primeiro = false
+    } else {
+        display.textContent += text
+    }
+
+    display.textContent = display.textContent.substring(0, 17)
+    numeroAtual = display.textContent
+    apagarIgual = true
+}
+
+const inserir = e => inserirDisplay(e.target.textContent)
+
+nums.forEach(e => e.addEventListener('click', inserir))
+
+const inserirOperador = e => {
+    primeiro = true
+    operador = e.target.textContent
+    if (operador == 'x') {
+        operador == '*'
+    } else if (operador == '÷') {
+        operador = '/'
+    }
+    numeroAnterior = display.textContent
+}
+
+operadores.forEach(e => e.addEventListener('click', inserirOperador))
+
+const calcular = () => {
+    if (numeroAnterior && operador) {
+        let result = numeroAnterior + operador
+
+        if(numeroAtual) {
+            result += numeroAtual
+        } else {
+            result += numeroAnterior
+        }
+
+        display.textContent = eval(result.replace(',', '.'))
+        ajustaPontoVirgula()
+
+        if (display.textContent == 'NaN') {
+            display.textContent = '0'
+        }
+
+        numeroAnterior = display.textContent
+        primeiro = true
+        apagarIgual = false
+    }
+}
+
+igual.addEventListener('click', calcular)
+
+const apagarUltimo = () => {
+    if (apagarIgual) {
+        if (display.textContent.length > 1) {
+            display.textContent = display.textContent.slice(0, -1)
+        } else {
+            display.textContent = 0
+        }
+
+        primeiro = true
+    }
+}
+
+apagar.addEventListener('click', apagarUltimo)
+
+const inverteSinal = () => {
+    display.textContent = parseFloat(display.textContent.replace(',', '.')) * -1
+    ajustaPontoVirgula()
+}
+
+maismenos.addEventListener('click', inverteSinal)
+
+const limpaCalculo = () => {
+    display.textContent = '0'
+    primeiro = true
+}
+
+limparCalculo.addEventListener('click', limpaCalculo)
+
+const limpaTudo = () => {
+    display.textContent = '0'
+    numeroAnterior = '0'
+    numeroAtual = '0'
+    primeiro = true
+}
+
+limparTudo.addEventListener('click', limpaTudo)
+
+const calcPorc = () => {
+    display.textContent = parseFloat(display.textContent.replace(',', '.')) / 100
+    ajustaPontoVirgula()
+    numeroAtual = display.textContent
+    primeiro = true
+}
+
+porc.addEventListener('click', calcPorc)
+
+const calcDivisaoUm = () => {
+    display.textContent = 1 / parseFloat(display.textContent.replace(',', '.'))
+    ajustaPontoVirgula()
+    numeroAtual = display.textContent
+    numeroAnterior = '0'
+    primeiro = true
+}
+
+divisaoUm.addEventListener('click', calcDivisaoUm)
+
+const calcPotencia = () => {
+    display.textContent = Math.pow(parseFloat(display.textContent.replace(',', '.')), 2)
+    ajustaPontoVirgula()
+    numeroAtual = display.textContent
+    numeroAnterior = '0'
+    primeiro = true
+}
+
+potencia.addEventListener('click', calcPotencia)
+
+const calcRaiz = () => {
+    display.textContent = Math.sqrt(parseFloat(display.textContent.replace(',', '.')))
+    ajustaPontoVirgula()
+    numeroAtual = display.textContent
+    numeroAnterior = '0'
+    primeiro = true
+}
+
+raiz.addEventListener('click', calcRaiz)
 
